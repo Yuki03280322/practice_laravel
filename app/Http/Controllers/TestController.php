46a71c384c8762f64dee134e842c737a6cecc9c3
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2021-02-27 18:02:28 +0900

    sample_view

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Test;
+
 class TestController extends Controller
 {
     //
+    public function index()
+    {
+        $values = Test::all();
+        // dd(die+var_dump)コマンド,処理を止めて変数の中身を表示する
+        // dd($value);
+
+        return view('tests.test', compact('values'));// compact関数＋変数で複数の変数でもまとめて渡すことができる
+    }
 }
