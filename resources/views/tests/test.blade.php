67f66671938f3b6110ea53621630b228efa89fe3
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2021-03-01 13:53:20 +0900

    laravel-ui,user登録スカフォールド,コンパイル

--- resources/views/tests/test.blade.php
+++ resources/views/tests/test.blade.php
@@ -4,3 +4,8 @@ Hello World!!<br>
 {{$value->id}}<br>
 {{$value->text}}<br>
 @endforeach
+{{-- 
+bladeはlaravelのテンプレートエンジンであり、viewの中にPHPを直接記述することが可能
+{{ }}記法はXSS攻撃を防ぐため自動的にPHPのhtmlspecialchars関数を通される
+bladeのメリットはレイアウト拡張と継承が可能な点
+  --}}
