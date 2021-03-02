5d54bc177f534e7bb6bf6688ebd40f733670987c
Yuki03280322 <72023246+Yuki03280322@users.noreply.github.com>
2021-03-02 17:14:12 +0900

    contact.createルーティング

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function() {
 // prefix => 'contact'このルートグループ内のURIを指定(URIにcontactをつけている)
 //middleware => auth:認証されていれば表示する(認証されていなければ認証(ログイン)画面へ)
     Route::get('index', 'ContactFormController@index')->name('contact.index');//nameはつけておくとviewファイル処理が楽に
+    Route::get('create', 'ContactFormController@create')->name('contact.create');
 });
 // Route::get('contact/index', 'ContactFormController@index'); contact/indexというパスが指定されたらContactFormControllerのindexアクションが起動
 Auth::routes();//ファサードクラスvender/laravel/framework/illuminate/Routing/Router.phpに記述されているauthメソッドを呼び出している
